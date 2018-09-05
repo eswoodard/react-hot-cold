bf2734b7939fc8b2b32aeaa5c8f827394fe99b86
Elizabeth Woodard <eswoodard75@gmail.com>
2018-09-05 15:24:12 -0400

    add redux

---
+++ src/components/guess-form.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {shallow, mount} from 'enzyme';
+
+
+import GuessForm from './guess-form';
+
+describe ('<GuessForm />', () => {
+  it('Renders without crashing', () => {
+    shallow(<GuessForm />);
+  });
+
+it('Renders guess form', () => {
+
+})
+
+});
